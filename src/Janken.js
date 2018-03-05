2250144628f88adc6ee6bee0cdc468d505aec169
Motoshi HORII <moro0229@gmail.com>
2018-03-05 23:57:22 +0900

    something is wrong

---
+++ src/Janken.js
@@ -0,0 +1,22 @@
+export default class Janken {
+	consructor(){
+		this.scores = []
+		this.statuses = [0, 0, 0]
+	}
+
+	pon(human_hand){
+		const computer_hand = Math.floor(Math.random() * 3)
+		const judgment = (computer_hand - human_hand + 3)%3
+
+		this.score.push({human: human_hand, computer: computer_hand, created_at: new Date(), judgment: judgment})
+		this.statuses[judgment]++
+	}
+
+	getScores(){
+		return this.score.slice().reverse()
+	}
+
+	getStatuses(){
+		return {draw: this.statuses[0], win: this.statuses[1], lose: this.statuses[2]}
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,58 +1,97 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import './index.css'
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import RaiseButton from 'material-ui/RaisedButton'
+import Paper from 'material-ui/Paper'
+
+import { Tabs, Tab } from 'material-ui/Tabs'
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table'
+import Janken from './Janken'
+
+// import './index.css'
 
 class JankenGamePage extends Component{
 	constructor(props){
 		super(props)
-		this.state = {human: null, computer: null}
-	}
-
-	pon(human_hand){
-		const computer_hand = Math.floor(Math.random() * 3)
-		this.setState({human: human_hand, computer: computer_hand})
-	}
-
-	judge(){
-		if( this.state.human == null ){
-			return null
-		}else{
-			return (this.state.computer - this.state.human + 3) % 3
-		}
+		this.janken = new Janken()
+		this.state = {scores: [], status: {}, tabIndex: 0}
 	}
 
 	componentDidMount(){
-		setTimeout(()=>{this.pon(1)}, 1000)
+		this.getResult()
 	}
 
-	shouldComponentUpdate(nextProps, nextState){
-		const identical = nextState.human == this.state.human && nextState.computer == this.state.computer
+	tabChange(ix){
+		this.setState({tabIndex: ix})
+		this.getResult()
+	}
 
-		if(identical){
-			console.log("*identical*")
-		}
+	getResult(){
+		this.setState({scores: this.janken.getScores()})
+		this.setState({status: this.janken.getStatuses()})
+	}
 
-		return !identical
+	pon(te){
+		this.janken.pon(te)
+		this.getReulst()
 	}
 
 	render(){
 		return(
-				<div>
-				<h1>じゃんけん　ぽん！</h1>
-				<JankenBox actionPon={(te) => this.pon(te)} />
-				<ScoreBox human={this.state.human} computer={this.state.computer} judgement={this.judge()} />
+				<MuiThemeProvider>
+				<div style={{marginLeft: 30}}>
+				<Header>じゃんけんぽん</Header>
+				<JankenBox actionPon={(te)=>this.pon(te)} />
+				<Paper style={{width: 400}} zDepth={2}>
+				<Tabs value={this.state.tabIndex} onChange={(ix)=>this.tabChange(ix)}>
+				<Tab label="対戦結果" value={0}>
+				<ScoreList scores={this.state.scores} />
+				</Tab>
+				<Tab label="対戦成績" value={1}>
+				<StatusBox status={this.state.status} />
+				</Tab>
+				</Tabs>
+				</Paper>
 				</div>
+				</MuiThemeProvider>
 		)
 	}
 }
 
+const Header = (props) => (<h1>{props.children}</h1>)
+Header.propTypes = {
+	children: PropTypes.string
+}
+
+const StatusBox = (props) => (
+		<Table>
+		<TableBody displayRowCheckbox={false}>
+		<TableRow displayBorder={false}>
+		<TableHeaderColumn>勝ち</TableHeaderColumn><TableRowColumn style={judgmentStyle(1)}>{props.status.win}</TableRowColumn>
+		</TableRow>
+		<TableRow displayBorder={false}>
+		<TableHeaderColumn>負け</TableHeaderColumn><TableRowColumn style={judgmentStyle(2)}>{props.status.lose}</TableRowColumn>
+		</TableRow>
+		<TableRow displayBorder={false}>
+		<TableHeaderColumn>引き分け</TableHeaderColumn><TableRowColumn style={judgmentStyle(0)}>{props.status.draw}</TableRowColumn>
+		</TableRow>
+		</TableBody>
+		</Table>
+)
+
+StatusBox.propTypes = {
+	status: PropTypes.object
+}
+
 const JankenBox = (props) => {
-	return (
-			<div>
-			<button onClick={() => props.actionPon(0)}>グー</button>
-			<button onClick={() => props.actionPon(1)}>チョキ</button>
-			<button onClick={() => props.actionPon(2)}>パー</button>
+	const style = {marginLeft: 20}
+	return(
+			<div style={{marginTop: 40, marginBotton: 30, marginLeft: 30}}>
+			<RaiseButton label="グー" onClick={()=>props.actionPon(0)} style={style} />
+			<RaiseButton label="チョキ" onClick={()=>props.actionPon(1)} style={style} />
+			<RaiseButton label="パー" onClick={()=>props.actionPon(2)} style={style} />
 			</div>
 	)
 }
@@ -61,31 +100,53 @@ JankenBox.propTypes = {
 	actionPon: PropTypes.func
 }
 
-const ScoreBox = (props) => {
-	const teString = ["グー","チョキ","パー"]
-	const judgementString = ["引き分け","勝ち","負け"]
+const ScoreList = (props) => (
+		<Table>
+		<TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+		<TableRow>
+		<TableHeaderColumn>時間</TableHeaderColumn><TableHeaderColumn>人間</TableHeaderColumn><TableHeaderColumn>コンピューター</TableHeaderColumn>
+		<TableHeaderColumn>結果</TableHeaderColumn>
+		</TableRow>
+		</TableHeader>
+		<TableBody>
+		{props.scores.map((score,ix)=><ScoreListItem key={ix} score={score} />)}
+	</TableBody>
+		</Table>
+)
 
-	return(
-			<table>
-			<tbody>
-			<tr><th>あなた</th><td>{teString[props.human]}</td></tr>
-			<tr><th>Computer</th><td>{teString[props.computer]}</td></tr>
-			<tr><th>勝敗</th><td>{judgementString[props.judgement]}</td></tr>
-			</tbody>
-			</table>
+ScoreList.propTypes={
+	scores: PropTypes.array
+}
+
+const ScoreListItem = (props) => {
+	const teString = ["グー", "チョキ", "パー"]
+	const judgmentString = ["引き分け", "勝ち", "負け"]
+	const dateHHMMSS = (d) => d.toTimeString().substr(0, 8)
+	return (
+			<TableRow style={judgmentStyle(props.score.judgment)}>
+			<TableRowColumn>{dateHHMMSS(props.score.created_at)}</TableRowColumn>
+			<TableRowColumn>{teString[props.score.human]}</TableRowColumn>
+			<TableRowColumn>{teString[props.score.computer]}</TableRowColumn>
+			<TableRowColumn>{judgmentString[props.score.judgment]}</TableRowColumn>
+			</TableRow>
 	)
 }
 
-ScoreBox.propTypes = {
-	human: PropTypes.number,
-	computer: PropTypes.number,
-	judgement: PropTypes.number
+ScoreListItem.propTypes = {
+	score: PropTypes.object
 }
+
+
+const judgmentStyle = (judgment) => ({color: ["#000", "#2979FF", "#FF1744"] [judgment]})
 
 ReactDOM.render(
 		<JankenGamePage />,
-	document.getElementById('root')
+		document.getElementById('root')
 )
+
+
+
+
 
 
 
